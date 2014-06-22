e55838da1756aaab7bbc9f7cb62ef3dd09fcf199
Federico Tomassetti <f.tomassetti@gmail.com>
2014-06-22 19:04:55 +0100

    Implementing the calc of lines changed

--- pydiffparser/parsing.py
+++ pydiffparser/parsing.py
@@ -1,22 +1,70 @@
+# Diff Parser, produce Diff representations.
 class Parser:
 
+    # Changes on a single file, represents
+    # eiher a new version or an old version of the file
+    # That is because sometimes the diff are between two different
+    # files.
+    class Changes:
+
+        def __init__(self, fname):
+            self.fname = fname
+            self.lines_changed = []
+
+    # Represent the complete Diff
     class Diff:
 
         def __init__(self):
             self.sections = []
 
+        def new(self, filename):
+            m = {}
+            for s in self.sections:
+                if s.fname_new:
+                    c = Parser.Changes(s.fname_new)
+                    c.lines_changed = s.lines_changed_on_new()
+                    return c
+            raise Exception("File not changes %s" % filename)
+
+    # Represent a single section, i.e. a comparison between two files
+    # (or two versions of the same file)
     class Section:
         
         def __init__(self):
             self.hunks = []
 
+        def lines_changed_on_new(self):
+            lines = []
+            for h in self.hunks:
+                lines.extend(h.lines_changed_on_new())         
+            return lines
+
+    # A block of lines of a single pair of files being diffed
     class Hunk:
+
+        def __init__(self):
+            self.old_lines = {}
+            self.new_lines = {}
         
         def lines_old(self):
             return self.end_old - self.start_old + 1
 
         def lines_new(self):
             return self.end_new - self.start_new + 1
+
+        def store_old(self, number, changed, line_content):
+            self.old_lines[number] = (changed, line_content)
+
+        def store_new(self, number, changed, line_content):
+            self.new_lines[number] = (changed, line_content)
+
+        def lines_changed_on_new(self):
+            lines = []
+            for k in self.new_lines:
+                changed, line_content = self.new_lines[k]
+                if changed:
+                    lines.append(k)               
+            return lines
 
     class SectionParser:
         
@@ -58,14 +106,24 @@ class Parser:
             old_toread = hunk.lines_old()
             new_toread = hunk.lines_new()
             cur = i+1
+            next_old = hunk.start_old
+            next_new = hunk.start_new
             while old_toread>0 or new_toread>0:
                 if lines[cur].startswith(" "):
                     old_toread -= 1
                     new_toread -= 1
+                    hunk.store_old(next_old, False, lines[cur][1:])
+                    hunk.store_new(next_new, False, lines[cur][1:])
+                    next_old   += 1
+                    next_new   += 1
                 elif lines[cur].startswith("+"):
                     new_toread -= 1
+                    hunk.store_new(next_new, True, lines[cur][1:])
+                    next_new   += 1
                 elif lines[cur].startswith("-"):
                     old_toread -= 1
+                    hunk.store_old(next_old, True, lines[cur][1:])
+                    next_old   += 1
                 else:
                     raise Exception("Line %i" % cur)
                 cur+=1
--- tests/parsing_tests.py
+++ tests/parsing_tests.py
@@ -1,11 +1,21 @@
 from nose.tools import *
-import pydiffparser
+from pydiffparser.parsing import *
 
 def setup():
-    print "SETUP!"
+    pass
 
 def teardown():
-    print "TEAR DOWN!"
+    pass
 
 def test_basic():
-    print "I RAN!"
+	p = Parser()
+	diff = p.parse("tests/data/ex2.diff")
+	eq_(len(diff.sections), 1, msg="1 Section expected")
+	eq_(diff.sections[0].fname_old, "03trail_fname.from")
+	eq_(diff.sections[0].fname_new, "03trail_fname.to")
+	eq_(len(diff.sections[0].hunks), 1)
+
+def test_changed_lines():
+	p = Parser()
+	diff = p.parse("tests/data/ex2.diff")
+	eq_(diff.new("03trail_fname.to").lines_changed, [4, 5])
