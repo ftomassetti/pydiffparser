a9dfdfbc673217de7073f41d0b3f083f0c48c330
Federico Tomassetti <f.tomassetti@gmail.com>
2014-06-23 22:15:28 +0100

    test: added test_changed_lines_on_old_file

--- tests/parsing_tests.py
+++ tests/parsing_tests.py
@@ -15,7 +15,12 @@ def test_basic():
 	eq_(diff.sections[0].fname_new, "03trail_fname.to")
 	eq_(len(diff.sections[0].hunks), 1)
 
-def test_changed_lines():
+def test_changed_lines_on_new_file():
 	p = Parser()
 	diff = p.parse("tests/data/ex2.diff")
 	eq_(diff.new_lines("03trail_fname.to"), [4, 5])
+
+def test_changed_lines_on_old_file():
+	p = Parser()
+	diff = p.parse("tests/data/ex2.diff")
+	eq_(diff.old_lines("03trail_fname.from"), [3])
