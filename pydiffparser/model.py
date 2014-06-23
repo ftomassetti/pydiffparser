451f862b110a863fe0f5553ab6ff9b7373243386
Federico Tomassetti <f.tomassetti@gmail.com>
2014-06-23 22:13:37 +0100

    model: adding missing specular methods and commenting

--- pydiffparser/model.py
+++ pydiffparser/model.py
@@ -26,6 +26,18 @@ class Diff:
     def new_lines(self, filename):
         return self.new(filename).lines_changed
 
+    def old(self, filename):
+        m = {}
+        for s in self.sections:
+            if s.fname_old:
+                c = Changes(s.fname_old)
+                c.lines_changed = s.lines_changed_on_old()
+                return c
+        raise Exception("File not changes %s" % filename)
+
+    def old_lines(self, filename):
+        return self.old(filename).lines_changed
+
 # Represent a single section, i.e. a comparison between two files
 # (or two versions of the same file)
 class Section:
@@ -33,10 +45,20 @@ class Section:
     def __init__(self):
         self.hunks = []
 
+    # List of lines changed on the new file.
+    # Line numbers are 1-based.
     def lines_changed_on_new(self):
         lines = []
         for h in self.hunks:
             lines.extend(h.lines_changed_on_new())         
+        return lines
+
+    # List of lines changed on the old file.
+    # Line numbers are 1-based.
+    def lines_changed_on_old(self):
+        lines = []
+        for h in self.hunks:
+            lines.extend(h.lines_changed_on_old())         
         return lines
 
 # A block of lines of a single pair of files being diffed
@@ -46,22 +68,42 @@ class Hunk:
         self.old_lines = {}
         self.new_lines = {}
     
+    # Number of lines of the old file which are
+    # considered in the hunk
     def lines_old(self):
         return self.end_old - self.start_old + 1
 
+    # Number of lines of the new file which are
+    # considered in the hunk
     def lines_new(self):
         return self.end_new - self.start_new + 1
 
+    # Save a line of the old file
+    # number  : 1-based index of the line
+    # changed : is the line different in respect to the other file
+    # line_content : full content of the line, without the newline
     def store_old(self, number, changed, line_content):
         self.old_lines[number] = (changed, line_content)
 
+    # Save a line of the new file
+    # number  : 1-based index of the line
+    # changed : is the line different in respect to the other file
+    # line_content : full content of the line, without the newline
     def store_new(self, number, changed, line_content):
         self.new_lines[number] = (changed, line_content)
 
+    # List of the list changed on the new file
     def lines_changed_on_new(self):
-        lines = []
-        for k in self.new_lines:
-            changed, line_content = self.new_lines[k]
+        return self.__lines_changed(self.new_lines)
+
+    # List of the list changed on the old file
+    def lines_changed_on_old(self):
+        return self.__lines_changed(self.old_lines)
+
+    def __lines_changed(self, lines):
+        changed_lines = []
+        for k in lines:
+            changed, line_content = lines[k]
             if changed:
-                lines.append(k)               
-        return lines
+                changed_lines.append(k)               
+        return changed_lines
